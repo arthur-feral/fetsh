9f92e39ab3ccbdc98f4a9bc3a5d171af4d6e2800
arthur-feral <arthur.feral@hotmail.fr>
2019-03-16 02:18:59 +0100

    feat(Fetsh): export all needed functions

---
+++ lib/index.ts
@@ -0,0 +1,18 @@
+import { RequestParameters, UrlContract } from './types';
+
+import request, {
+  createUrlContract as _createUrlContract,
+} from './request';
+
+export const get = (urlContract: UrlContract, requestParameters?: RequestParameters) =>
+  request('get', urlContract, requestParameters);
+export const post = (urlContract: UrlContract, requestParameters?: RequestParameters) =>
+  request('post', urlContract, requestParameters);
+export const put = (urlContract: UrlContract, requestParameters?: RequestParameters) =>
+  request('put', urlContract, requestParameters);
+export const patch = (urlContract: UrlContract, requestParameters?: RequestParameters) =>
+  request('patch', urlContract, requestParameters);
+export const del = (urlContract: UrlContract, requestParameters?: RequestParameters) =>
+  request('delete', urlContract, requestParameters);
+export * from './parameters';
+export const createUrlContract = _createUrlContract;
